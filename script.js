6547a6b59b9e54fc09b073bae2a4abeab12c22a4
DanielKimmothy <166509424+DanielKimmothy@users.noreply.github.com>
2024-04-16 10:05:20 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let Dave = 0
+/* let Dave = 0
 let upgrade = 1
 let doublecost
 let getID = "document.getElementById"
@@ -6,43 +6,12 @@ let cost = 100
 
 document.getElementById("doublebutton").onclick = function(){
     if (Dave > 50){
-        /* not working
         upgrade = 2 * upgrade
-        if (Dave > cost){
-            doublecost = cost / 2
-        } 
-        else if (Dave > 2 * cost){
-            doublecost = 2 * cost / 2
-        }
-        else if (Dave > 3 * cost){
-            doublecost = 3 * cost / 2
-        }
-        else if (Dave > 5 * cost){
-            doublecost = 5 * cost / 2
-        }
-        else if (Dave > 6 * cost){
-            doublecost = 6 * cost / 2
-        }
-        else if (Dave > 7 * cost){
-            doublecost = 7 * cost / 2
-        }
-        else if (Dave > 8 * cost){
-            doublecost = 8 * cost / 2
-        }
-        else if (Dave > 9 * cost){
-            doublecost = 9 * cost / 2
-        }
-        else if (Dave > 10 * cost){
-            doublecost = 10 * cost / 2
-        }
-        */
-
-
-        Dave = Dave - Dave / 2
+        Dave = (Dave - Dave / 2)
         document.getElementById("number").innerHTML = Dave
     }
     else {
-        document.getElementById("!").innerHTML = "You need " + -(Dave - 50)  + " more points to purchase this item."
+        document.getElementById("!").innerHTML = "You need " + -(Dave - 50)  + " more points to unlock this item."
     }
 }
 
